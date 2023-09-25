4f41ec0278923cd712fd5fb636605d48fccb242d
Marcelo Araya-Salas <marcelo.araya@ucr.ac.cr>
2023-09-25 16:24:47 -0600

    final fix to html colors

--- render_quarto_files.R
+++ render_quarto_files.R
@@ -1,8 +1,11 @@
 # install.packages("quarto")
 styler::style_dir(filetype = "qmd")
 
-rmds <- list.files(pattern = "qmd$")
 
-for (i in rmds) {
-  try(quarto::quarto_render(i), silent = TRUE)
-}
+quarto::quarto_render()
+
+# rmds <- list.files(pattern = "qmd$")
+# 
+# for (i in rmds) {
+#   try(quarto::quarto_render(i), silent = TRUE)
+# }
