a3372769f611ff4e783754195f0b1460654c5fee
Marcelo Araya-Salas <marcelo.araya@ucr.ac.cr>
2023-09-28 09:01:28 -0600

    remove date index

--- render_quarto_files.R
+++ render_quarto_files.R
@@ -1,11 +1,4 @@
 # install.packages("quarto")
 styler::style_dir(filetype = "qmd")
 
-
 quarto::quarto_render()
-
-# rmds <- list.files(pattern = "qmd$")
-# 
-# for (i in rmds) {
-#   try(quarto::quarto_render(i), silent = TRUE)
-# }
